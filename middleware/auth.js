34b56827b74772b1b519c3f84665446ab0cb2415
SARMISTHA PAL <spp123.micro@gmail.com>
2021-10-17 22:59:26 +0530

    Auth Config Changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   //Get token from header
-  const token = req.header('x-auth-token');
+  const authToken = req.header('x-auth-token');
 
   //Check if no token
-  if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied ' });
+  if (!authToken) {
+    return res.status(401).json({ msg: 'Auth token is null, authorization denied ' });
   }
 
   //Verify token
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(authToken, config.get('jwtSecret'));
 
     // @ts-ignore
     req.user = decoded.user;
